0db6c1a16303a30725711d49c0cf4e73d260d17f
Yujin Takayanagi <yujin.takayanagi@internetbrands.com>
2023-02-21 00:17:03 -0800

    fix: revert React version to 17

--- container/ContactPage/ContactForm/ContactForm.tsx
+++ container/ContactPage/ContactForm/ContactForm.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import * as React from 'react'
+
 import { useForm } from '@formspree/react'
 import { CheckIcon } from '@heroicons/react/24/solid'
 import ReCAPTCHA from 'react-google-recaptcha'
